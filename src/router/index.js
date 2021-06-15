a4f02ef79979759e4eb7ddce0f476e331e321de4
unknown <1593809042@qq.com>
2021-06-15 10:59:12 +0800

    15号中午

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,15 @@ import VueRouter from 'vue-router'
 import Index from '../components/Index/index.vue'
 import Search from '../components/Index/search.vue'
 import Stay from '../components/Index/homestay.vue'
+import mapIndex from '../components/Index/mapIndex.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {path:'/',component:Index},
   {path:'/search',component:Search},
-  {path:'/stay',component:Stay}
+  {path:'/stay',component:Stay},
+  {path:'/mapIndex',component:mapIndex}
 ]
 
 const router = new VueRouter({
