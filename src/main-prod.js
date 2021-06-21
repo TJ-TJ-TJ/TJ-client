4eca0d78f3e05d69101ee269c6a6498eb98f2bee
zhao1985019435@gmail.com <zhao1985019435@gmail.com>
2021-06-22 01:38:13 +0800

    完善订单功能前

--- src/main-prod.js
+++ src/main-prod.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
+// import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Vant from 'vant';
+// import Vant from 'vant';
 import 'vant/lib/index.css';
 import VueSocketIO from 'vue-socket.io'
 import ClientSocketIO from 'socket.io-client'
@@ -11,7 +11,7 @@ import TableBar from './components/tableBar.vue'  //全局的底部tablebar组�
 
 
 import { Lazyload } from 'vant';
-import { List } from 'vant';
+// import { List } from 'vant';
 import { Toast } from 'vant';
 // vue-touch
 import VueTouch from "vue-touch";
@@ -19,9 +19,9 @@ Vue.use(VueTouch, { name: "v-touch" });
 
 Vue.component("table-bar", TableBar); // 直接使用即可 <table-bar></table-bar>
 
-Vue.use(Vant);
-Vue.use(List);
-Vue.use(Lazyload);
+// Vue.use(Vant);
+// Vue.use(List);
+// Vue.use(Lazyload);
 axios.defaults.validateStatus = status=> true
 axios.defaults.baseURL = 'https://tj.testw.top/v1'
 Vue.prototype.$axios=axios
